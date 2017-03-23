8a190161e14074ed017b9878a8e6305d520cde6e
Rushil Bhuptani <rushil.bhuptani55@gmail.com>
2017-03-23 14:07:36 +0530

    Modified files

--- iasset technical test.Tests/Controllers/HomeControllerTest.cs
+++ iasset technical test.Tests/Controllers/HomeControllerTest.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using iasset_technical_test;
 using iasset_technical_test.Controllers;
+using iasset_technical_test.Services;
+using Moq;
 
 namespace iasset_technical_test.Tests.Controllers
 {
@@ -17,12 +19,27 @@ namespace iasset_technical_test.Tests.Controllers
         {
             // Arrange
             HomeController controller = new HomeController();
-
+           
             // Act
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void Test()
+        {
+            string cityName = "Australia";
+            // Arrange
+            HomeController controller = new HomeController();
+            var weatherData = new Mock<IWeatherData>();
+            var cities = new List<string>();
+            Api.HomeController homeController = new Api.HomeController(weatherData.Object);
+            weatherData.Setup(x => x.GetCitiesData(cityName)).Returns(cities);
+
+            var response = homeController.GetCities(cityName);
+           
+            Assert.IsNotNull(response);
         }
     }
 }
